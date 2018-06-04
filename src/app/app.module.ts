6e2daf4cc97a8fdda46447a4494568eb2c914f42
John Theo <john.theo.souza@gmail.com>
2018-06-04 02:08:29 -0300

    Instalação do Froala Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Import Froala Editor.
+import "froala-editor/js/froala_editor.pkgd.min.js";
+
+// Import Angular2 plugin.
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    FroalaEditorModule.forRoot(), 
+    FroalaViewModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import * as $ from 'jquery'; 
+window["$"] = $; 
+window["jQuery"] = $;
+
 platformBrowserDynamic().bootstrapModule(AppModule);
+
