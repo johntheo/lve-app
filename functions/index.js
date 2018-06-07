1c3f718ca4bcbda88519b067b0c61c6b329c59a5
John Theo <john.theo.souza@gmail.com>
2018-06-07 02:23:01 -0300

    Saving in the firebase database
    The behavior isn't correct. Need to set the data, not push

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,42 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const cors = require('cors')({origin: true});
+
 //Function that receive a rich text and save into database
 exports.saveText = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.content;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    return admin.database().ref('/text').push({original: original}).then((snapshot) => {
-      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-      return res.redirect(303, snapshot.ref.toString());
+    cors(req, res, () => {
+        // Grab the text parameter.
+        const id = req.body.id;
+        const type = req.body.type;
+        const content = req.body.content;
+        // Push the new message into the Realtime Database using the Firebase Admin SDK.
+        if(type == 'title'){
+            return admin.database().ref('/text/'+id).push({title: content}).then((snapshot) => {
+                // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+                return res.redirect(303, snapshot.ref.toString());
+            });
+        }else {
+            return admin.database().ref('/text/'+id).push({content: content}).then((snapshot) => {
+                // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+                return res.redirect(303, snapshot.ref.toString());
+            });
+        }
+        
     });
+
+    
   });
+
+//Creates a new empty text
+exports.createText = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        return admin.database().ref('/text/').push().then((snapshot) => {
+            // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+            return res.redirect(303, snapshot.ref.toString());
+        });
+    });
+});
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,13 +11,33 @@ export class HomePage {
     toolbarInline: true,
     toolbarButtons: ['bold', 'italic', 'insertLink','|', 'paragraphFormat', 'quote', 'clearFormatting', '|', 'undo', 'redo'],
     charCounterCount: false,
-    placeholderText: 'Título'
+    placeholderText: 'Título',
+    // Change save interval (time in miliseconds).
+    saveInterval: 2500,
+    // Set the save param.
+    saveParam: 'content',
+    // Set the save URL.
+    saveURL: 'https://us-central1-app-lve.cloudfunctions.net/saveText',
+    // HTTP request type.
+    saveMethod: 'POST',
+    // Additional save params.
+    saveParams: {id: 'a1',type: 'title'}
   }
 
   public contentOptions: Object = {
     toolbarInline: true,
     toolbarButtons: ['bold', 'italic', 'insertLink','|', 'paragraphFormat', 'quote', 'clearFormatting', 'formatOL', 'formatUL', 'outdent', 'indent','|', 'undo', 'redo'],
-    placeholderText: 'Respire e deixe fluir...'
+    placeholderText: 'Respire e deixe fluir...',
+    // Change save interval (time in miliseconds).
+    saveInterval: 2500,
+    // Set the save param.
+    saveParam: 'content',
+    // Set the save URL.
+    saveURL: 'https://us-central1-app-lve.cloudfunctions.net/saveText',
+    // HTTP request type.
+    saveMethod: 'POST',
+    // Additional save params.
+    saveParams: {id: 'a1',type: 'content' }
   }
 
   constructor(public navCtrl: NavController) {
